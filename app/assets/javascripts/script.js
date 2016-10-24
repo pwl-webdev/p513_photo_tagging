b413bf87ded691be8e40bf22abf306964c4b9f70
Pwl Webdev <pwl.webdev@gmail.com>
2016-10-24 22:53:22 +0200

    ajax tag query and response completed

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
 	console.log("script.js ready & loaded");
 	$('#image').click(function(e){
-        var posX = $(this).offset().left,
-            posY = $(this).offset().top;
-		console.log((e.pageX - posX) + ' , ' + (e.pageY - posY));
+        var posX = e.pageX - $(this).offset().left,
+            posY = e.pageY - $(this).offset().top;
+		console.log((posX) + ' , ' + (posY));
+		tagQuery(posX, posY);
 	});
 });
+
+var tagQuery = function(posX, posY){
+	//console.log($('#title').val());
+	$.ajax({
+		url: window.location.href,
+		data: {
+			posX: posX,
+			posY: posY
+		},
+		type: "GET",
+		dataType: "json",
+	}).done(function( json ) {
+    	console.log(json.status)
+    	if(json.status == "success"){
+    		alert("Congratulations!");
+    	}
+	}).fail(function( xhr, status, errorThrown ) {
+	   console.log( "Error: " + errorThrown );
+	   console.log( "Status: " + status );
+	   console.dir( xhr );
+	});
+};
--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -4,5 +4,29 @@ class PuzzlesController < ApplicationController
 	end
 	def show
 		@puzzle = Puzzle.find(params[:id])
+
+		respond_to do |format|
+			format.html
+			format.json {
+				puts "duuuupa"
+				puts params[:posX]
+				puts params[:posY]
+				@resp
+				if (params[:posX].to_i >= @puzzle.wally_x && params[:posX].to_i <= (@puzzle.wally_x + @puzzle.wally_dx) && params[:posY].to_i >= @puzzle.wally_y && params[:posY].to_i <= (@puzzle.wally_y + @puzzle.wally_dy)) then
+					puts "wally found!"
+					@resp = "success"
+				else 
+					puts "wally not found!"
+					@resp = "fail"
+				end
+				render json: {status: @resp}.to_json
+			}
+
+		end
 	end
+
+	#private 
+	#def puzzle_params
+	#	params.require().permit(:id, :posX, :posY)
+	#end
 end
