3dd91a04570e01102730aa7ae545ac7adcd2aa6a
Pwl Webdev <pwl.webdev@gmail.com>
2016-10-26 18:37:16 +0200

    Marker added with every click

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -4,9 +4,15 @@ $(document).ready(function(){
         var posX = e.pageX - $(this).offset().left,
             posY = e.pageY - $(this).offset().top;
 		console.log((posX) + ' , ' + (posY));
+		var target_box_X = e.pageX - 10;
+		var target_box_Y = posY - 10;
+		$(`<div class="target_box" id = "c${clickCounter}" style="top: ${target_box_Y}px; left: ${target_box_X}px;"></div>`).appendTo('#frame');
 		tagQuery(posX, posY);
+		clickCounter++;
 	});
 });
+
+var clickCounter = 0;
 
 var tagQuery = function(posX, posY){
 	//console.log($('#title').val());
@@ -21,7 +27,10 @@ var tagQuery = function(posX, posY){
 	}).done(function( json ) {
     	console.log(json.status)
     	if(json.status == "success"){
-    		alert("Congratulations!");
+    		$(".target_box").css("border-color","green");
+    		alert("Congratulations! Your time is "+json.time);
+    	} else{
+    		$(".target_box").fadeOut( "slow" );
     	}
 	}).fail(function( xhr, status, errorThrown ) {
 	   console.log( "Error: " + errorThrown );
--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -4,22 +4,24 @@ class PuzzlesController < ApplicationController
 	end
 	def show
 		@puzzle = Puzzle.find(params[:id])
-
 		respond_to do |format|
-			format.html
+			format.html {
+				@start_time = Time.now
+			}
 			format.json {
-				puts "duuuupa"
-				puts params[:posX]
-				puts params[:posY]
 				@resp
 				if (params[:posX].to_i >= @puzzle.wally_x && params[:posX].to_i <= (@puzzle.wally_x + @puzzle.wally_dx) && params[:posY].to_i >= @puzzle.wally_y && params[:posY].to_i <= (@puzzle.wally_y + @puzzle.wally_dy)) then
 					puts "wally found!"
 					@resp = "success"
+					@end_time = Time.now
+					#render json: {status: @resp, time: (@end_time - @start_time)}.to_json
+					render json: {status: @resp}.to_json
 				else 
 					puts "wally not found!"
 					@resp = "fail"
+					render json: {status: @resp}.to_json
 				end
-				render json: {status: @resp}.to_json
+
 			}
 
 		end
